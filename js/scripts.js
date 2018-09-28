c87861f17b68a3ec5676b607a830a41ad16f29b8
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-27 21:35:52 -0700

    add the missing quotation marks for value gathering from inputs to operate

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,20 @@ var multiples = [];
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    var finalNum = parseInt($("#number1").val());
+    var multipleNum = parseInt($("#number2").val());
 
-    var finalNum = parseInt($(#number1).val());
-    var multipleNum = parseInt($(#number2).val());
 
-    if (typeof finalNum !== "number" || typeof multipleNum !== "number" || finalNum < 0 || multipleNum < 0) {
-      $("#output-sentence").text("Please enter whole, positive itegers for your numbers to be calculated.");
+    if (typeof finalNum !== "number" || typeof multipleNum !== "number" || finalNum < 0 || multipleNum < 0 || $("#number1").val() === "" || $("#number2").val() === "") {
+      $("#output-sentence").text("Please enter whole, positive imtegers for your numbers to be calculated.");
     } else if (multipleNum > finalNum) {
       $("#output-sentence").text("Your desginated multiple is larger than your requested final number. Please enter a multiple that is smaller than your final number.")
-    } else {
-      for (index=0, index <= finalNum, index += multipleNum) {
+    } else if (typeof finalNum === "number" && typeof multipleNum === "number") {
+      for (index=0; index <= finalNum; index += multipleNum) {
         multiples.push([index]);
       };
-      $("#output-sentence").text("To get up to " + final +", counting up by " + multiple + "s:");
-      $("#output-numbers").text(counting);
+      $("#output-sentence").text("To get up to " + finalNum +", counting up by " + multipleNum + "s:");
+      $("#output-numbers").text(multiples);
     };
   });
 });
